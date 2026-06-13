b667560ebb427d8e1e89d061a2b1cd455894cbe9
Waliullah <waliullah.godev@gmail.com>
2026-06-13 12:46:03 +0600

    admin class

--- user.py
+++ user.py
@@ -20,4 +20,16 @@ class Employee(User):
         self.salary = salary
 
 
-emp = Employee("Rahim", "rahim@example.com", "1234567890", "123 Main St", 30, "Manager", 50000)
+
+# emp = Employee("Rahim", "rahim@example.com", "1234567890", "123 Main St", 30, "Manager", 50000)
+# print(emp.name)
+
+
+
+
+class Admin(User):
+    def __init__(self, name, email, phone, address):
+        super().__init__(name, email, phone, address)
+        self.employees = []
+
+    
