d77f5f47ba369d89352ea5828857fae092afd284
Waliullah <waliullah.godev@gmail.com>
2026-06-13 12:51:38 +0600

    3 instance

--- user.py
+++ user.py
@@ -32,12 +32,20 @@ class Admin(User):
         super().__init__(name, email, phone, address)
         self.employees = []
 
-    def add_employee(self, name, email, phone, address):
-        employee = Employee(name, email, phone, address)
+    def add_employee(self, name, email, phone, address, age, designation, salary):
+        employee = Employee(name, email, phone, address, age, designation, salary)
         self.employees.append(employee) 
         print(f"Employee {name} added successfully.")
 
     def view_employees(self):
         print("List of Employees:")
         for employee in self.employees:
-            print(f"Name: {employee.name}, Email: {employee.email}, Phone: {employee.phobe}, Address: {employee.address}")
+            print(f"Name: {employee.name}, Email: {employee.email}, Phone: {employee.phobe}, Address: {employee.address}, Age: {employee.age}, Designation: {employee.dasignation}, Salary: {employee.salary}")
+
+
+
+ad = Admin("Admin", "admin@example.com", "0987654321", "456 Admin St")
+ad.add_employee("Rahim", "rahim@example.com", "1234567890", "123 Main St", 30, "Manager", 50000)
+ad.add_employee("Karim", "karim@example.com", "0987654321", "456 Main St", 25, "Developer", 40000)
+ad.add_employee("Jamal", "jamal@example.com", "1111111111", "789 Main St", 28, "Designer", 45000)
+ad.view_employees()
