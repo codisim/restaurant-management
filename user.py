e1c439cc64bb7475b3bfd87df09ae6b5e09dff84
Waliullah <waliullah.godev@gmail.com>
2026-06-23 13:46:59 +0600

    order module

--- user.py
+++ user.py
@@ -43,31 +43,7 @@ class Customer(User):
         
         
 
-class Order():
-    def __init__(self):
-        self.items = {}
-        
-    def add_item(self, item):
-        if item in self.items:
-            self.items[item] += item.quantity
-        else:
-            self.items[item] = item.quantity
-            
-    def remove_item(self, item):
-        if item in self.items:
-            del self.items[item]
-        else:
-            print(f"Item '{item.name}' not found in the cart.")
-            
-    
-    def total_price(self):
-        return sum(item.price * quantity for item, quantity in self.items.items()) 
-    
-    def clear(self):
-        self.items = {}
-        
-        
-        
+
         
 
 # Employee class to represent an employee of the restaurant
