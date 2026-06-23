870b6641b947b285d334ce2accd5119536134eb4
Waliullah <waliullah.godev@gmail.com>
2026-06-23 13:46:11 +0600

    menu module

---
+++ meny.py
@@ -0,0 +1,32 @@
+
+# Menu class to represent a menu item
+class Menu():
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+
+    def find_item(self, item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+        return None
+
+    def remove_item(self, item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print(f"Item '{item_name}' removed from the menu.")
+        else:
+            print(f"Item '{item_name}' not found in the menu.")
+            
+            
+    def view_menu(self):
+        print("Menu Items:")
+        print("Name\tPrice\tQuantity")
+        for item in self.items:
+            print(f"{item.name}\t{item.price}\t{item.quantity}")
+            
+    
