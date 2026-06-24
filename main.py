7de853d674b608507fc9ccba33362322acec1d57
Waliullah <waliullah.godev@gmail.com>
2026-06-24 13:35:49 +0600

    admin instance

--- main.py
+++ main.py
@@ -40,3 +40,40 @@ def customer_menu():
         elif choice == 5:
             print("Thank you for visiting Mamar's Restaurant!")
             break
+        
+
+
+
+def admin_menu():
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    phone = input("Enter your phone number: ")
+    address = input("Enter your address: ")
+    
+    admin = Admin(name, email, phone, address)
+    
+    
+    while True:
+        print(f"Welcome, {admin.name}!!")
+        print("1. Add new item")
+        print("2. Add  new employee")
+        print("3. View employees")
+        print("4. View items")
+        print("5. Delete item")
+        print("6. Exit")
+        
+        
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            mamar_restaurant.view_menu()
+        elif choice == 2:
+            item_name = input("Enter the name of the item to add to cart: ")
+            quantity = int(input("Enter the quantity: "))
+            mamar_restaurant.add_to_cart(admin, item_name, quantity)
+        elif choice == 3:
+            mamar_restaurant.view_cart(admin)
+        elif choice == 4:
+            mamar_restaurant.pay_bill(admin)
+        elif choice == 5:
+            print("Thank you for visiting Mamar's Restaurant!")
+            break
